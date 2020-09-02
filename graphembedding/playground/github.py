65e72cfcc36184256938ba6ce44fa397004bf92a
craftsangjae <craftsangjae@gmail.com>
2020-09-02 12:46:05 +0900

    k-core sampling을 utils.py로 빼고, playground.github 내 k-core sampling 제거

--- graphembedding/playground/github.py
+++ graphembedding/playground/github.py
@@ -4,12 +4,10 @@ Author : SangJae Kang
 Mail : craftsangjae@gmail.com
 """
 import pandas as pd
-from tqdm import tqdm
 
 
 def load_github(name='linux',
-                event_types=("WatchEvent", 'PushEvent', 'IssuesEvent'),
-                k_core=5):
+                event_types=("WatchEvent", 'PushEvent', 'IssuesEvent')):
     """knowledge graph Dataset을 불러오는 함수
     현재 3가지 github knowledge graph가 구성되어 있음
     name : (linux, tensorflow, vim)
@@ -25,7 +23,7 @@ def load_github(name='linux',
     target_df = pd.read_hdf(fpath, key=name)
 
     type_df = pd.read_hdf(fpath, key='type')
-    target_df.type = target_df.type.map(type_df.type.to_dict())
+    target_df.type = target_df.type.map(type_df['type'].to_dict())
 
     repository_df = pd.read_hdf(fpath, key='repository')
     df = pd.merge(target_df, repository_df)
@@ -37,32 +35,8 @@ def load_github(name='linux',
 
     # 필요한 event type만 가져오기
     df = trim_relations(df, event_types)
-
-    # K-core Sampling 수행
-    df = kcore_sampling(df, k_core)
     return df
 
 
 def trim_relations(df, event_types):
     return df[df.relation.isin(event_types)]
-
-
-def kcore_sampling(df, k_core=5):
-    print("Start K-core Sampling", flush=True)
-    pbar = tqdm()
-
-    while True:
-        prev_counts = len(df)
-        if prev_counts == 0:
-            raise ValueError("No data remains")
-
-        sub_counts = df.subject.value_counts()
-        obj_counts = df.object.value_counts()
-        df = df[df.subject.isin(sub_counts[sub_counts >= k_core].index)
-                & df.object.isin(obj_counts[obj_counts >= k_core].index)]
-
-        if prev_counts == len(df):
-            # 변화가 없으면 종료
-            return df
-        pbar.update(1)
-    return df
